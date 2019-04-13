7502a6d1eded1fc2cf9f628bcea7ce6f11147b1c
Metts <metts@Mettss-MacBook-Pro.local>
2019-04-13 17:24:16 -0500

    add band reds

---
+++ battle.js
@@ -0,0 +1,21 @@
+// console.log("test");
+
+let bandNumber = 0
+
+const takeNumber = function (bandName) {
+    bandNumber += 1
+    console.log(bandNumber + " " + bandName)
+
+
+}
+
+const scum = takeNumber("Galactic Scum")
+  // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs")
+ // This should print "2. Underdogs" in the console
+
+const strays = takeNumber("Strays")
+
+const reds = takeNumber("Reds")
+
